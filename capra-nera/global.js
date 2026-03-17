21e873cb87e731376bc431757636c3fe502554dc
Jochem te Pas <jochemtepas@Mac.fritz.box>
2026-03-17 19:20:33 +0100

    Fix: add padding to SplitText mask to prevent clipping on ascenders

--- capra-nera/global.js
+++ capra-nera/global.js
@@ -363,8 +363,10 @@ function initHeadingReveal() {
   );
 
   const allLines = splits.flatMap((s) => s.lines);
+  const allMasks = allLines.map((line) => line.parentElement);
 
   gsap.set(headings, { autoAlpha: 1 });
+  gsap.set(allMasks, { paddingTop: "0.15em", marginTop: "-0.15em" });
   gsap.set(allLines, { y: 100, skewY: 7 });
 
   gsap.to(allLines, {
