76cb1a9281ed43b2dde79bc1bc4368df72846501
Jochem te Pas <jochemtepas@Mac.fritz.box>
2026-06-25 20:19:06 +0200

    yieldfund: nav — weer ingeschakeld

--- yieldfund/global.js
+++ yieldfund/global.js
@@ -1387,7 +1387,7 @@ function initAll() {
   initBasicFormValidation();
   initNavDropdowns();
   initPhotoRowsMarquee();
-  // initFixedUnderlayNavigation();
+  initFixedUnderlayNavigation();
 }
 
 // ==========================================================
